88fbfdb143aa8b0bfcf68580dc9e9601873159ab
baconYao <bacon735392@gmail.com>
2019-06-20 22:11:30 +0800

    Add some listeners in Service Worker

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,22 @@
+// service worker
 'use strict';
 
-// service worker
+// 加入 install event listener
+self.addEventListener("install", function(event) {
+  console.log("[Service Worker] Installing Service Worker...", event);
+});
+
+// 加入 activate event listener
+self.addEventListener("activate", function(event) {
+  console.log("[Service Worker] Activating Service Worker...", event);
+  return self.clients.claim();
+});
+
+// 加入fetch event listener
+self.addEventListener("fetch", function(event) {
+  /* fetch包含 image src, css, js 等resources的讀取 */
+  console.log("[Service Worker] Fetching something...", event);
+  // overwrite the response
+  // event.respondWith(null);    // 每當有fetch被trigger時，都回傳null值
+  event.respondWith(fetch(event.request));     // 有fetch被trigger時，取得真實的resources
+});
